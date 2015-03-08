e128c3eaa4dfd427991b5770e7df5c70baae389a
Daniel Treasure <dbtreasure@gmail.com>
2015-03-08 04:58:04 -0600

    reinvents the countdown to a graphical display

--- public/app.js
+++ public/app.js
@@ -2,17 +2,27 @@ var app = angular.module('myApp', ['angularMoment']);
 
 app.controller( 'MainCtrl', function( $scope, $timeout, moment) {
   $scope.timeLeft;
-  nextShowing = moment().weekday(1).hour(2).minute(35);
+
+  $scope.goToEp1 = function(){
+    console.log('goToEp1');
+  }
 
   $scope.onTimeout = function(){
-    (function() {
-      // nextShowing = moment().weekday(1).hour(2).minute(15);
-      var ms = nextShowing.diff(moment());
-      var d = moment.duration(ms);
-      var s = Math.floor(d.asDays()) + moment.utc(ms).format(":hh:mm:ss");
 
-      $scope.timeLeft = s;
-    }());
+      //if today is tuesday, adjust the moment's day value
+      if (moment().day() === 3){
+        //if the time is past the start time set it to next week
+        if(moment().set({'hours': 10, 'minutes': 59, 'seconds': 00}) - moment() <= 0){
+          nextShowing = moment().set({'day': '10', 'hours': 10, 'minutes': 59, 'seconds': 00});
+        //otherwise set it to today
+        } else{
+          nextShowing = moment().set({'day': '3', 'hours': 10, 'minutes': 59, 'seconds': 00});
+        }
+      }
+
+      var secondsLeft = Math.floor(nextShowing.diff(moment()) / 1000);
+
+      $scope.timeLeft = secondsLeft;
 
     mytimeout = $timeout($scope.onTimeout,1000);
   }
