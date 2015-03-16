f562e6b542410244803aa1a995e2a0e941d5fee0
Daniel Treasure <dbtreasure@gmail.com>
2015-03-15 21:37:32 -0600

    adjustments to a daily showing

--- public/app.js
+++ public/app.js
@@ -3,28 +3,26 @@ var app = angular.module('myApp', ['angularMoment']);
 app.controller( 'MainCtrl', function( $scope, $timeout, moment) {
   $scope.timeLeft;
 
+  $scope.portalOpen = false;
+
   $scope.goToEp1 = function(){
     console.log('goToEp1');
   }
 
+  $scope.goToEp2 = function(){
+    console.log('goToEp2');
+  }
+
   $scope.onTimeout = function(){
 
-      //if today is tuesday, adjust the moment's day value
-      if (moment().day() === 3){
-        //if the time is past the start time set it to next week
-        if(moment().set({'hours': 10, 'minutes': 59, 'seconds': 00}) - moment() <= 0){
-          nextShowing = moment().set({'day': '10', 'hours': 10, 'minutes': 59, 'seconds': 00});
-        //otherwise set it to today
-        } else{
-          nextShowing = moment().set({'day': '3', 'hours': 10, 'minutes': 59, 'seconds': 00});
-        }
-      }
+    nextShowing = moment().set({'day': '0', 'hours': 23, 'minutes': 00, 'seconds': 00});
 
-      var secondsLeft = Math.floor(nextShowing.diff(moment()) / 1000);
+    var secondsLeft = Math.floor(nextShowing.diff(moment()) / 1000);
 
-      $scope.timeLeft = secondsLeft;
+    $scope.timeLeft = secondsLeft;
 
     mytimeout = $timeout($scope.onTimeout,1000);
+
   }
 
   var mytimeout = $timeout($scope.onTimeout,1000);
