d73ba307a6b64e75eb28c63ac6ef1e4d908be97c
Daniel Treasure <dbtreasure@gmail.com>
2015-03-15 23:06:51 -0600

    stupid router is being stupid whatever

--- public/app.js
+++ public/app.js
@@ -1,17 +1,32 @@
-var app = angular.module('myApp', ['angularMoment']);
+var app = angular.module('myApp', ['angularMoment', 'ngRoute'])
 
-app.controller( 'MainCtrl', function( $scope, $timeout, moment) {
+app.config(function($routeProvider, $locationProvider){
+  $routeProvider
+  .when('/', {
+    templateUrl : 'views/index.html',
+    controller: 'MainCtrl'
+  })
+  .when('/ep1', {
+    templateUrl : 'views/episode1.html',
+    controller: 'MainCtrl'
+  })
+  .when('/ep2', {
+    templateUrl : 'views/episode2.html',
+    controller: 'MainCtrl'
+  });
+
+  $locationProvider.html5Mode(true);
+})
+
+app.controller( 'MainCtrl', function( $scope, $timeout, moment, $location, $route) {
   $scope.timeLeft;
 
   $scope.portalOpen = false;
 
-  $scope.goToEp1 = function(){
-    console.log('goToEp1');
-  }
-
-  $scope.goToEp2 = function(){
-    console.log('goToEp2');
-  }
+  $scope.go = function ( path ) {
+    $location.path( path );
+    $route.reload();
+  };
 
   $scope.onTimeout = function(){
     nextShowing = moment().set({'day': '0', 'hours': 21, 'minutes': 00, 'seconds': 00});
